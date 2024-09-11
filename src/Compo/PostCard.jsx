99041099aa2cd8aa4d91099491b8035770b4ac34
MyTution <rohitsonawane889@gmail.com>
2024-09-11 13:23:38 +0530

    BigBlog

--- src/Compo/PostCard.jsx
+++ src/Compo/PostCard.jsx
@@ -12,7 +12,7 @@ function PostCard({$id, title, featuredImage}) {
                 className='rounded-xl' />
 
             </div>
-            <h2
+            <h2  
             className='text-xl font-bold'
             >{title}</h2>
         </div>
