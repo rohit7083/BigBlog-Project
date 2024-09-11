c2ada94ac6c5f598233604f7fb4b2f40413333da
MyTution <rohitsonawane889@gmail.com>
2024-09-11 16:00:30 +0530

    BigBlog

--- src/Compo/index.js
+++ src/Compo/index.js
@@ -3,11 +3,13 @@ import Footer from './Footer/Footer'
 import Container from './container/Container'
 import Logo from './Logo'
 import Logoutbtn from './Header/Logoutbtn'
+import RTE from './RTE'
 export{
     Header,
     Footer,
     Container,
     Logo,
     Logoutbtn,
+    RTE,
     
 }
